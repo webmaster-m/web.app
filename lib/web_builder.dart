b5c2f44ce6aa8deb80cd1eb5860ea383e171b258
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2020-01-05 06:55:43 +0530

    chore: builder working perfectly fine
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- lib/web_builder.dart
+++ lib/web_builder.dart
@@ -3,11 +3,8 @@ library code_generators;
 import 'package:build/build.dart';
 import 'package:mrigesh.dev/src/copy_builder.dart';
 
-Builder copyBuilder(BuilderOptions options) => CopyBuilder();
+Builder noopBuilder(_) => NoopBuilder();
 
 PostProcessBuilder cleanupUnneededFiles(BuilderOptions options) =>
-    FileDeletingBuilder([
-      '.md',
-      '.yaml',
-      '.lock',
-    ]);
+    FileDeletingBuilder([''],
+        isEnabled: options.config['enabled'] as bool ?? true);
